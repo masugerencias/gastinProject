dbdb9aafca449848de33694ea6154c2d73447093
Yann Poirot sanz <ypoirot@beonit.es>
2024-03-30 09:42:38 +0100

    fix: vue router fixed working 100%

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,25 +1,28 @@
-import { createRouter, createWebHistory} from "vue-router";
-import FirstPage from '../views/FirstPage';
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '../views/Home';
-Vue.use(Router)
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import FirstPage from '../views/FirstPage.vue';
 
+Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: Home
-    },
-    {
-        path: '/firstPage',
-        name: 'firstPage',
-        component: FirstPage
-    },
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/firstPage',
+    name: 'firstPage',
+    component: FirstPage
+  },
+];
 
-]
-
-const router = createRouter({history: createWebHistory('/'), routes})
+// Y aquí, cambia la inicialización del router a la forma de Vue 2:
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+});
 
 export default router;
